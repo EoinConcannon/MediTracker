049dc991ebdd368a1deefa6e013e1baadc8378d8
Eoin Concannon <caneoin20@gmail.com>
2026-07-18 13:26:11 +0100

    Removed setup code, added config.js

---
+++ client/src/config.js
@@ -0,0 +1,5 @@
+export const PATIENT_SERVICE_URL = 'http://localhost:8081';
+export const VITALS_SERVICE_URL = 'http://localhost:8084';
+export const APPOINTMENT_SERVICE_URL = 'http://localhost:8082';
+export const MEDICATION_SERVICE_URL = 'http://localhost:8083';
+export const NOTIFICATION_SERVICE_URL = 'http://localhost:8085';
