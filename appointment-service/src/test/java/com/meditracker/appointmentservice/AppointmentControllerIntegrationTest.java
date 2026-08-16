1cdab268086f6d6e6e63c8c21dfdc4ab23615d04
Eoin Concannon <caneoin20@gmail.com>
2026-08-16 13:06:51 +0100

    fixing integration tests

--- appointment-service/src/test/java/com/meditracker/appointmentservice/AppointmentControllerIntegrationTest.java
+++ appointment-service/src/test/java/com/meditracker/appointmentservice/AppointmentControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class AppointmentControllerIntegrationTest {
 
 	@Autowired
--- patient-service/src/test/java/com/meditracker/patientservice/PrescriptionControllerIntegrationTest.java
+++ patient-service/src/test/java/com/meditracker/patientservice/PrescriptionControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class PrescriptionControllerIntegrationTest {
 
 	@Autowired
