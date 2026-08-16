db3a90cdac14e58a380e7e4ed2c3d4b55e7ef765
Eoin Concannon <caneoin20@gmail.com>
2026-08-16 13:16:43 +0100

    fixing integration tests again

--- patient-service/src/test/java/com/meditracker/patientservice/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/meditracker/patientservice/PatientServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.meditracker.patientservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PatientServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
