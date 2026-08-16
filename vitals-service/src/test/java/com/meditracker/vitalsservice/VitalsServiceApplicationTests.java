3b6e3dfe187c57a476c033ef9a541ae136b72d17
Eoin Concannon <caneoin20@gmail.com>
2026-08-16 13:52:02 +0100

    testing application.properties for integration tests

--- vitals-service/src/test/java/com/meditracker/vitalsservice/VitalsServiceApplicationTests.java
+++ vitals-service/src/test/java/com/meditracker/vitalsservice/VitalsServiceApplicationTests.java
@@ -4,12 +4,10 @@ import com.meditracker.vitalsservice.service.NotificationServiceClient;
 import com.meditracker.vitalsservice.service.PatientServiceClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class VitalsServiceApplicationTests {
 
 	@MockitoBean
